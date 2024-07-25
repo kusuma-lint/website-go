196132192a73ab81bb1e148358310db53122a2b9
kusuma-lint <kusuma.lestari98@gmail.com>
2024-07-25 14:45:34 +0700

    [added] instal html bootstap I

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,10 +1,19 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/unrolled/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Website E-commerce Home Page")
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	_ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
+		"title": "Home Title",
+		"body":  "Home Description",
+	})
+	// fmt.Fprintf(w, "Welcome to Website E-commerce Home Page")
 }
