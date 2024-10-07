b0522291371827b1022f28f28dbf2500c29d87d3
kusuma-lint <kusuma.lestari98@gmail.com>
2024-10-07 11:30:54 +0700

    create-db-connection-to-postgre

--- app/controllers/routes.go
+++ app/controllers/routes.go
@@ -9,12 +9,6 @@ import (
 func (server *Server) initializeRoutes() {
 	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", server.Home).Methods("GET")
-	server.Router.HandleFunc("/products", server.Products).Methods("GET")
-	server.Router.HandleFunc("/products/{slug}", server.GetProductBySlug).Methods("GET")
-
-	server.Router.HandleFunc("/carts", server.GetCart).Methods("GET")
-	server.Router.HandleFunc("/carts", server.AddItemToCart).Methods("POST")
-
 	staticFileDirectory := http.Dir("./assets/")
 	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
 	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
